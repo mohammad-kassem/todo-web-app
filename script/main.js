5ae7216d8320ed9d5608b06ed8e08b5d9b55e168
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-29 12:40:19 +0300

    adding the create todo template pop up JS

--- script/main.js
+++ script/main.js
@@ -1,6 +1,31 @@
-function createTodo() {
-  let title = $("#title").val();
-  console.log(title);
-}
+let todos = [];
 
-createTodo();
+$("#add-todo-button").click(function () {
+  if ($(".create")) {
+    $(".create").remove();
+  }
+  $create_todo_template = `<div class="todo create">
+  <div class="todo-container">
+      <div>
+          <input type="checkbox" name="todo-done" disabled>
+      </div>
+      <div class="todo-content">
+          <i id="cancel" class="fa-solid fa-xmark"></i>
+          <textarea id="title" class="title" placeholder="Title..."></textarea>
+          <textarea id="description" class="description" placeholder="Description..."></textarea>
+          <div>
+              <input type="radio" id="1" name="points" value="1">
+              <input type="radio" id="2" name="points" value="2">
+              <input type="radio" id="3" name="points" value="3">
+              <input type="radio" id="4" name="points" value="4">
+              <input type="radio" id="5" name="points" value="5">
+          </div>
+          <button id="save-todo" class="btn"> Save Todo </button>
+      </div>
+  </div>
+</div>`;
+  $("#add-todo-container").append($create_todo_template);
+  $("#cancel").click(function () {
+    $(".create").remove();
+  });
+});
