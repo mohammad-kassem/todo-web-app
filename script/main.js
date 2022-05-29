eeda1446135a18e0376c2dd05aded4601e395cc6
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-29 16:05:33 +0300

    adding the sorting the todos by date and by points JS

--- script/main.js
+++ script/main.js
@@ -1,4 +1,6 @@
 $todos_array = [];
+$is_sorted_by_date = true;
+$is_sorted_by_points = false;
 
 $("#add-todo-button").click(function () {
   if ($(".create")) {
@@ -35,22 +37,30 @@ function saveTodo() {
   let title = $("#title").val();
   let description = $("#description").val();
   let points = $('input[name="points"]:checked').val();
-  let id = Math.ceil(Math.random() * 10);
+  let id = Math.ceil(Math.random() * 1000);
   let date = new Date();
-  let date_created =
-    String(date.getDate()) +
-    "/" +
-    String(date.getMonth()) +
-    "/" +
-    String(date.getFullYear());
-  let new_todo = [id, title, description, points, date_created, false];
+  let new_todo = [id, title, description, points, date, false];
   $todos_array.push(new_todo);
   $(".create").remove();
-  displayTodos();
+  displayTodosByDate();
 }
 
 function displayTodos() {
+  n = $todos_array.length;
+  for (let i = 0; i < n; i++) {
+    $(".todo").remove();
+  }
   $todos_array.forEach((element) => {
+    let date_created =
+      String(element[4].getDate()) +
+      "/" +
+      String(element[4].getMonth() + 1) +
+      "/" +
+      String(element[4].getFullYear()) +
+      " " +
+      String(element[4].getHours()) +
+      ":" +
+      String(element[4].getMinutes());
     $todo_item = `<div class="todo">
     <div class="todo-container">
         <div class="todo-content">
@@ -61,19 +71,54 @@ function displayTodos() {
             <textarea class="title" placeholder="Title..."> ${element[1]}</textarea>
             <textarea class="description" placeholder="Description...">  ${element[2]} </textarea>
             <div>
-                <input type="radio" name="points" value="1">
-                <input type="radio" name="points" value="2">
-                <input type="radio" name="points" value="3">
-                <input type="radio" name="points" value="4">
-                <input type="radio" name="points" value="5">
+                <input type="radio" name= "points${element[0]}"  value="1">
+                <input type="radio" name="points${element[0]}" value="2">
+                <input type="radio" name="points${element[0]}" value="3">
+                <input type="radio" name="points${element[0]}" value="4">
+                <input type="radio" name="points${element[0]}" value="5">
             </div>
-            <p> ${element[4]}</p>
+            <p> ${date_created}</p>
         </div>
     </div>
   </div>`;
     $("#active").append($todo_item);
-    $(`input[name=points][value=${element[3]}]`).prop("checked", true);
+    $(`input[name=points${element[0]}][value=${element[3]}]`).prop(
+      "checked",
+      true
+    );
   });
 }
 
-displayTodos();
+function compareDates(a, b) {
+  return b[4].getTime() - a[4].getTime();
+}
+
+function comparePoints(a, b) {
+  return b[3] - a[3];
+}
+
+function displayTodosByDate() {
+  $todos_array.sort(compareDates);
+  displayTodos();
+}
+
+function displayTodosByPoints() {
+  $todos_array.sort(comparePoints);
+  displayTodos();
+}
+
+$("#date-sort").click(function () {
+  if (!$is_sorted_by_date) {
+    $is_sorted_by_date = true;
+    $is_sorted_by_points = false;
+    displayTodosByDate();
+  }
+});
+
+$("#points-sort").click(function () {
+  if (!$is_sorted_by_points) {
+    $is_sorted_by_date = false;
+    $is_sorted_by_points = true;
+    displayTodosByPoints();
+  }
+});
